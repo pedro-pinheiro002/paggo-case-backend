c7a31a9c704f85874b3a49cd7e85f2908d6066ec
pedro-pinheiro002 <pedropinheiroborges2@gmail.com>
2024-07-17 01:37:59 -0300

    feat: create S3 service

---
+++ src/database/s3.service.ts
@@ -0,0 +1,15 @@
+import { S3Client } from '@aws-sdk/client-s3';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class S3Service {
+  constructor(readonly configService: ConfigService) {}
+  readonly s3Client = new S3Client({
+    region: this.configService.get('AWS_REGION'),
+    credentials: {
+      accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+    },
+  });
+}
